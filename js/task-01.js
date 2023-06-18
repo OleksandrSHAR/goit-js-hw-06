a7045ddc8f6b489561486d8202ae2a5049cfa997
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2023-06-18 13:00:29 +0300

    goit-js-hw-06

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+
+const itemEl = document.querySelectorAll(`.item`)
+console.log("Number of categories:", itemEl.length)
+
+itemEl.forEach(function (item){
+    console.log("Category:", item.children[0].textContent,)
+
+    console.log("Elements:", item.children[1].children.length)
+})
